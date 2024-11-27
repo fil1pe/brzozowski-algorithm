eaaae7fdc65dd16a0dbe3c67c16b8a847220a8f5
Filipe Ramos <ik.ben.filipe@gmail.com>
2024-11-27 08:31:02 -0300

    Fix examples

--- Examples.v
+++ Examples.v
@@ -1,7 +1,7 @@
-Require Export Brzozowski.
+Require Export Brzozowski_complete.
 
 Definition nfa1 := [start 0; transition 0 0 0; accept 0].
 Definition nfa2 := [start 0; transition 0 1 1; transition 0 0 2; transition 1 1 1; transition 1 0 2; accept 0; accept 1].
 
-Compute det_nfa PeanoNat.Nat.eq_dec PeanoNat.Nat.eq_dec nfa1.
-Compute det_nfa PeanoNat.Nat.eq_dec PeanoNat.Nat.eq_dec nfa2.
+Compute brzozowski PeanoNat.Nat.eq_dec PeanoNat.Nat.eq_dec nfa1.
+Compute brzozowski PeanoNat.Nat.eq_dec PeanoNat.Nat.eq_dec nfa2.
